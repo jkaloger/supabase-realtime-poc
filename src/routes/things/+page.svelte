cbffb86f214865e8de19cb1cb285f45e38dfa69f
Jack Kaloger <jkaloger@users.noreply.github.com>
2024-01-07 14:29:02 +1100

    style ✨

--- src/routes/things/+page.svelte
+++ src/routes/things/+page.svelte
@@ -1,7 +1,10 @@
 <!-- src/routes/profile/+page.svelte -->
 
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
 	import * as Card from '$lib/components/ui/card';
+	import { Badge } from '$lib/components/ui/badge';
 	import type { RealtimePostgresInsertPayload } from '@supabase/supabase-js';
 	import type { Database } from '../../DatabaseDefinitions.js';
 
@@ -23,27 +26,24 @@
 </script>
 
 <form method="POST" action="?/add_thing">
-	<input type="text" name="name" />
-	<button>Add Thing :)</button>
+	<div class="mx-auto my-4 flex w-1/2 flex-col gap-2">
+		<Input type="text" placeholder="Thing Name" name="name" />
+		<Button>Add Thing :)</Button>
+	</div>
 </form>
 
-<h1>Things</h1>
-<div>
-	{#if things}
-		{#each things as thing}
-			<Card.Root>
-				<Card.Header>
-					<Card.Title>{thing.name}</Card.Title>
-					<Card.Description>Card Description</Card.Description>
-				</Card.Header>
-				<Card.Content>
-					<p>Card Content</p>
-				</Card.Content>
-				<Card.Footer>
-					<p>Card Footer</p>
-				</Card.Footer>
-			</Card.Root>
-		{/each}
-	{/if}
+<div class="m-4 grid grid-cols-2 gap-4">
+	<div class="flex flex-col gap-2">
+		{#if things}
+			{#each things as thing}
+				<Card.Root>
+					<Card.Header>
+						<Card.Title>{thing.name}</Card.Title>
+						<Card.Description><Badge>{thing.created_by}</Badge></Card.Description>
+					</Card.Header>
+				</Card.Root>
+			{/each}
+		{/if}
+	</div>
 </div>
 <pre>{JSON.stringify(user, null, 2)}</pre>
